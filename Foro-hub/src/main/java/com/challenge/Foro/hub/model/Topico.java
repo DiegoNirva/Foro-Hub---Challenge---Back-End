69613922b6ce1048e36d3e1e06571b39093e7b69
DiegoNirva <diegonvz96@hotmail.com>
2025-01-08 22:25:43 -0300

    Se agregan clases de modelado

---
+++ Foro-hub/src/main/java/com/challenge/Foro/hub/model/Topico.java
@@ -0,0 +1,19 @@
+package com.challenge.Foro.hub.model;
+
+import java.time.LocalDateTime;
+
+public class Topico {
+
+
+    long id;
+    String titulo;
+    String mensaje;
+    LocalDateTime fechaDeCracion;
+    int status;
+    String autor;
+    String curso;
+
+
+
+
+}
---
+++ Foro-hub/src/main/java/com/challenge/Foro/hub/model/Usuario.java
@@ -0,0 +1,11 @@
+package com.challenge.Foro.hub.model;
+
+public class Usuario {
+
+    long id;
+    String nombre;
+    String correo;
+    String contrasenia;
+    
+
+}
