bc3fa04c1160132a5964da37f8fc0d026e4112a0
DiegoNirva <diegonvz96@hotmail.com>
2025-01-12 20:24:20 -0300

    Se crea clases usuarios y DTOs

---
+++ Foro-hub/src/main/java/com/challenge/Foro/hub/DTO/DatosTopico.java
@@ -0,0 +1,7 @@
+package com.challenge.Foro.hub.DTO;
+
+public record DatosTopico(String titulo,
+        String mensaje,
+        String autor,
+        String curso) {
+}
--- Foro-hub/src/main/java/com/challenge/Foro/hub/model/Usuario.java
+++ Foro-hub/src/main/java/com/challenge/Foro/hub/model/Usuario.java
@@ -1,8 +1,25 @@
 package com.challenge.Foro.hub.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "usuario")
+@EqualsAndHashCode(of = "id")
 public class Usuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     long id;
+
+
     String nombre;
     String correo;
     String contrasenia;
