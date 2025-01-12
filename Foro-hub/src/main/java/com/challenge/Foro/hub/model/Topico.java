b733b0a6ce2ca45316a8d66679ffcd7f6f432b9a
DiegoNirva <diegonvz96@hotmail.com>
2025-01-12 20:23:18 -0300

    Se modifica clase topico

--- Foro-hub/src/main/java/com/challenge/Foro/hub/model/Topico.java
+++ Foro-hub/src/main/java/com/challenge/Foro/hub/model/Topico.java
@@ -1,18 +1,51 @@
 package com.challenge.Foro.hub.model;
 
-import java.time.LocalDateTime;
+import com.challenge.Foro.hub.DTO.DatosTopico;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "topico")
+@EqualsAndHashCode(of = "id")
 public class Topico {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     long id;
+
     String titulo;
     String mensaje;
-    LocalDateTime fechaDeCracion;
+    String fechaDeCreacion;
     int status;
     String autor;
     String curso;
 
+    public Topico(DatosTopico datosTopico){
+        this.titulo = datosTopico.titulo();
+        this.mensaje = datosTopico.mensaje();
+        this.fechaDeCreacion = fechaFormateada();
+        this.status = 1; //se utilizaran 1 para true y 0 para false
+        this.autor = datosTopico.autor();
+        this.curso = datosTopico.autor();
+
+    }
+
+    //metodo para formatear fecha
+    private String fechaFormateada(){
+        LocalDateTime fechaActual = LocalDateTime.now();
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String fechaFormateada = fechaActual.format(formato);
+        return  fechaFormateada;
+    }
 
 
 
